c7b9647f236f6565dfb4eec13b3f25a78661732c
leunghon <leunghon5@gmail.com>
2024-01-11 12:04:41 -0800

    database migration part 1

--- klet/klet/settings.py
+++ klet/klet/settings.py
@@ -69,7 +69,6 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = "klet.wsgi.application"
 
 
@@ -78,8 +77,12 @@ WSGI_APPLICATION = "klet.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": BASE_DIR / "db.mysql",
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # Or your database server (e.g., AWS RDS endpoint)
+        'PORT': '3306s',
     }
 }
 
