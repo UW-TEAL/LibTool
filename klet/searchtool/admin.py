3bab26aa4ea094be89610cb501ccd1df4af164e2
leunghon <leunghon5@gmail.com>
2024-01-16 13:18:20 -0800

    admin portal view

--- klet/searchtool/admin.py
+++ klet/searchtool/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ['authorKorean', 'authorEnglish', 'workTitle', 'genre', 'translator', 'sourceTitle', 'publisher', 'year', 'yearCreated', 'authorEnglish2', 'uid2']
+    list_filter = ['authorKorean', 'authorEnglish', 'workTitle', 'genre', 'translator', 'sourceTitle', 'publisher', 'year', 'yearCreated', 'authorEnglish2', 'uid2']
+    search_fields = ['authorKorean', 'authorEnglish', 'workTitle', 'genre', 'translator', 'sourceTitle', 'publisher', 'year', 'yearCreated', 'authorEnglish2', 'uid2']
+    list_per_page = 10
+
 admin.site.register(Record)
