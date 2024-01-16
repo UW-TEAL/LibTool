81a8e4979b76078ddd4e8073c6af2e0b236c6349
leunghon <leunghon5@gmail.com>
2024-01-16 13:25:39 -0800

    admin portal change

--- klet/searchtool/admin.py
+++ klet/searchtool/admin.py
@@ -4,9 +4,8 @@ from django.contrib import admin
 from .models import *
 
 class ModelAdmin(admin.ModelAdmin):
-    list_display = ['authorKorean', 'authorEnglish', 'workTitle', 'genre', 'translator', 'sourceTitle', 'publisher', 'year', 'yearCreated', 'authorEnglish2', 'uid2']
-    list_filter = ['authorKorean', 'authorEnglish', 'workTitle', 'genre', 'translator', 'sourceTitle', 'publisher', 'year', 'yearCreated', 'authorEnglish2', 'uid2']
+    list_display = ['authorKorean', 'authorEnglish', 'workTitle', 'genre', 'translator', 'sourceTitle', 'publisher', 'year']
     search_fields = ['authorKorean', 'authorEnglish', 'workTitle', 'genre', 'translator', 'sourceTitle', 'publisher', 'year', 'yearCreated', 'authorEnglish2', 'uid2']
-    list_per_page = 10
+    list_per_page = 20
 
 admin.site.register(Record, ModelAdmin)
