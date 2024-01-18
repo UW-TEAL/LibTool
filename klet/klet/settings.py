3cf27a3393e9af3617ffeebdee9e1ad94037a78b
leunghon <leunghon5@gmail.com>
2024-01-18 10:47:11 -0800

    test import function

--- klet/klet/settings.py
+++ klet/klet/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     "django_filters",
     "searchtool",
+    'import_export'
     #Default
     "django.contrib.admin",
     "django.contrib.auth",
--- klet/searchtool/admin.py
+++ klet/searchtool/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 from .models import *
 
-class ModelAdmin(admin.ModelAdmin):
+class ModelAdmin(admin.ModelAdmin, ImportExportModelAdmin):
     list_display = ['authorKorean', 'authorEnglish', 'workTitle', 'genre', 'translator', 'sourceTitle', 'publisher', 'year']
     search_fields = ['authorKorean', 'authorEnglish', 'workTitle', 'genre', 'translator', 'sourceTitle', 'publisher', 'year', 'yearCreated', 'authorEnglish2', 'uid2']
     list_per_page = 20
