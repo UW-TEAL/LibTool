715a80127f444edc31889518e7b8e1e0a4630794
leunghon <leunghon5@gmail.com>
2024-01-18 12:35:37 -0800

    admin portal import through excel

--- klet/searchtool/admin.py
+++ klet/searchtool/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 from .models import *
+from .resources import RecordResource
 
 
 class ModelAdmin(ImportExportModelAdmin):
+    resource_class = RecordResource
     list_display = ['authorKorean', 'authorEnglish', 'workTitle', 'genre', 'translator', 'sourceTitle', 'publisher', 'year']
     search_fields = ['authorKorean', 'authorEnglish', 'workTitle', 'genre', 'translator', 'sourceTitle', 'publisher', 'year', 'yearCreated', 'authorEnglish2', 'uid2']
     list_per_page = 20
