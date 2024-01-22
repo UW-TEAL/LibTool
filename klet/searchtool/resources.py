6f8012e3401b0233ba0181c8e67e5b4fa5e9fba9
leunghon <leunghon5@gmail.com>
2024-01-22 12:49:49 -0800

    modified:   klet/searchtool/resources.py

--- klet/searchtool/resources.py
+++ klet/searchtool/resources.py
@@ -5,4 +5,5 @@ class RecordResource(resources.ModelResource):
     class Meta:
         model = Record
         # Specify fields to exclude from the import
+        import_id_fields = ['authorKorean', 'authorEnglish', 'workTitle', 'genre', 'translator', 'sourceTitle', 'publisher', 'year']
         exclude = ('id', 'yearCreated', 'authorEnglish2', 'uid2')
