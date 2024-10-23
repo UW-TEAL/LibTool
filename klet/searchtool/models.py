c5ea9226004c28dd5f44c0e0b0834a1f1665ca63
Tom <tom@nustechnology.com>
2024-10-23 14:12:27 +0700

    Fix text in admin

--- klet/searchtool/models.py
+++ klet/searchtool/models.py
@@ -11,6 +11,9 @@ class AuthorEnglish(models.Model):
 	name = models.CharField(max_length=255)
 	first_name = models.CharField(max_length=255)
 	last_name = models.CharField(max_length=255)
+	class Meta:
+			verbose_name = "Author English"
+			verbose_name_plural = "Author English"
 	def __str__(self):
 		return self.name
 
